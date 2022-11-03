c12836b025d06aa48d3b42dc4f77975ad1fa6e0e
movy18x <movy18x@gmail.com>
2022-11-03 20:11:55 +0300

    connect on mongoDBcloud

--- src/seeders/index.js
+++ src/seeders/index.js
@@ -4,14 +4,19 @@ require('dotenv').config()
 const LOCALHOST = process.env.HOST;
 const DB_PORT = process.env.PORT;
 const DATABASE = process.env.DATABASE;
+const MONGODB_CLOUD_CON = process.env.MONGODB_CLOUD_CON;
 
 const seed = require('./rolesSeeder')
-mongoose.connect(`mongodb://${LOCALHOST}:${DB_PORT}/${DATABASE}`);
-const db = mongoose.connection;
-db.on('error',(e)=>{
-    console.error(e)
-})
 
+if(process.env.ENVIRONMENT == 'development')
+{
+    mongoose.connect(`mongodb://${LOCALHOST}:${DB_PORT}/${DATABASE}`);
+}
+else{
+    console.log(MONGODB_CLOUD_CON)
+    mongoose.connect(MONGODB_CLOUD_CON);
+}
+const db = mongoose.connection;
 db.once('open', async ()=>{
     console.log('Connected to DB successfully')
     console.log('Seeding Started')
